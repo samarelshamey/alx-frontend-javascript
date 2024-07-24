8035ffc8afb282ee675df0c9d7df41b02da8411c
root <smsmelshamey90@gmail.com>
2024-07-24 20:57:19 +0000

    promise

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,9 +5,9 @@ export default async function handleProfileSignup(firstName, lastName, fileName)
   return Promise
     .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then((result) => (
-      result.map((o) => ({
-        status: o.status,
-        value: o.status === 'fulfilled' ? o.value : String(o.reason),
+      result.map((r) => ({
+        status: r.status,
+        value: r.status === 'fulfilled' ? r.value : String(r.reason),
       }))
     ));
 }
