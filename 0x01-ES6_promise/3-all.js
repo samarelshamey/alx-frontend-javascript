3efc0bf3c84a76b7a5feb08cebdb353f14bc8e5b
root <smsmelshamey90@gmail.com>
2024-07-24 19:02:22 +0000

    promise

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,10 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  Promise
+    .all([uploadPhoto(), createUser()])
+    .then(([photo, user]) => {
+      console.log(`${photo} ${user.firstName} ${user.lastName}`);
+    })
+    .catch(() => console.log('Signup system offline'));
+}
