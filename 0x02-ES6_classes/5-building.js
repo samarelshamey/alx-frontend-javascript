cd75a31124b280e32bb806edcd6467d95c5b6ffd
root <smsmelshamey90@gmail.com>
2024-07-25 21:15:39 +0000

    classes

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,15 @@
+export default class Building {
+	constructor(sqft) {
+		this.sqft = sqft;
+	}
+	get sqft() {
+		return this._sqft;
+	}
+	set sqft(value) {
+		this._sqft = value;
+	}
+	evacuationWarningMessage() {
+		throw new Error('Class extending Building must override evacuationWarningMessage');
+	}
+
+}
