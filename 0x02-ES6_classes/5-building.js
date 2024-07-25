00a4ceb5d6b9d398999a1ffa1a366194f938615d
root <smsmelshamey90@gmail.com>
2024-07-25 21:44:42 +0000

    classes

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,16 +1,24 @@
 export default class Building {
-	constructor(sqft) {
-		this.sqft = sqft;
-		if (this.constructor !== Building) {
-			if (typeof this.evacuationWarningMessage !== 'function') {
-				throw new Error('Class extending Building must override evacuationWarningMessage');
-			}
-		}
-	}
-	get sqft() {
-		return this._sqft;
-	}
-	set sqft(value) {
-		this._sqft = value;
-	}
+  constructor(sqft) {
+    this.sqft = sqft;
+    this.override();
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(value) {
+    this._sqft = value;
+  }
+
+  override() {
+    if (this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+  }
+
+  evacuationWarningMessage() {
+    throw new Error('evacuationWarningMessage must be implemented in subclass');
+  }
 }
