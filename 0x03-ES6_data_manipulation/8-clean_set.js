76b7db83372ed6981957806bf97b7ba2d41e34ed
root <smsmelshamey90@gmail.com>
2024-07-29 11:37:51 +0000

    Data Manipulation

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,7 @@
+export default function cleanSet(set, startString) {
+  const filtered = Array.from(set)
+    .filter((value) => typeof value === 'string' && value.startsWith(startString))
+    .map((value) => value.slice(startString.length));
+
+  return filtered.join('-');
+}
